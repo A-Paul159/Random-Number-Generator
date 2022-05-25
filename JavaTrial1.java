79129695711d1ffef3e59eda168237ed5b36cb7b
A-Paul159 <bilgamrinderpal@gmail.com>
2022-05-25 07:45:02 -0700

    Add files via upload

--- JavaTrial1.java
+++ JavaTrial1.java
@@ -1,33 +1,55 @@
-/*
-Generate 5 random numbers
- */
-
-
+import java.util.Arrays;
+import java.util.Scanner;
 public class JavaTrial1 {
     public static void main(String[] args) {
-        System.out.println("The 5 randomly generated numbers are:");
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("How many unique numbers would you like: ");
+        int Length = myObj.nextInt();
+        System.out.println("What is the upper bound for the numbers: ");
+        int Upper = myObj.nextInt();
 
-
-        double Lotto[] = new double[5];
+        double Lotto[] = new double[Length];
         int i = 0;
         double x = 0;
-        while (i < 5) {
+        int a = 0;
+        int b = 0;
+                    /* Create a loop to fill the array with the random numbers*/
+        while (i < Length) {
+                    /* Green light parameters*/
+            a = 0;
+            b = 1;
+                    /* Create a positive random 2 digit number*/
             x = Math.random();
             x = x * 100;
             x = Math.round(x);
-            if (x < 30) {
+                    /* If the number is in the range [0,Upper] the first parameter is green*/
+            if (x > 1 && x <= Upper) {
+                a = 1;
+            }
+                    /* If the number is distinct from the others the second parameter is green*/
+            int k = 1;
+            while (k <= i) {
+                if (x == Lotto[i-k]) {
+                    b = 0;
+                    System.out.println("---" + x + " was randomly generated a second time so it was removed.---");
+                    break;
+                }
+                k++;
+            }
+                    /* If both parameters are green, record the number in array and move to the next number*/
+            if (a == 1 && b == 1) {
                 Lotto[i] = x;
                 i++;
             }
         }
-        System.out.print(Lotto[0] + ", ");
-        System.out.print(Lotto[1] + ", ");
-        System.out.print(Lotto[2] + ", ");
-        System.out.print(Lotto[3] + ", ");
-        System.out.print(Lotto[4] + ", ");
-
-
+                    /* Run a loop to print each element from the array because I didn't know how to do it all at once*/
+        System.out.println("The " + Length + " randomly generated numbers are:");
+        /*for (int j = 0; j < Length; j++) {
+            System.out.print(Lotto[j] + " ");
+        }*/
+        System.out.println(Arrays.toString(Lotto));
 
     }
-    }
+}
+
 
